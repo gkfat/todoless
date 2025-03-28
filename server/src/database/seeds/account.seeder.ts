a4e23def218dec1eac6796dd1f1bcad4a157cad9
gkfat <gkgkdesign@gmail.com>
2025-03-28 15:55:48 +0800

    refactor(server): 重構 account seeder

--- server/src/database/seeds/account.seeder.ts
+++ server/src/database/seeds/account.seeder.ts
@@ -6,61 +6,65 @@ import { AccountAuth } from 'src/modules/accounts/entities/account-auth.entity';
 import { Account } from 'src/modules/accounts/entities/account.entity';
 import { Role } from 'src/modules/privileges/entities/role.entity';
 import { hashPassword } from 'src/utils/credential';
-import { DataSource } from 'typeorm';
+import {
+    DataSource,
+    Repository,
+} from 'typeorm';
 import { Seeder } from 'typeorm-extension';
+
+const seedRoles = async (roleRepository: Repository<Role>) => {
+    const roles = Object.values(EnumRole);
+
+    for (const role of roles) {
+        const newRole = new Role({ role });
+        await roleRepository.upsert(newRole, ['role']);
+    }
+};
+
+const seedSuperAccount = async (accountRepository: Repository<Account>, roleRepository: Repository<Role>) => {
+    const name = 'super';
+    const email = process.env.SUPER_EMAIL || 'super@todoless.com';
+    const password = process.env.SUPER_PASSWORD ?? 'super123';
+
+    const superRole = await roleRepository.findOneBy({ role: EnumRole.SUPER });
+
+    const accountAuth = new AccountAuth({
+        type: EnumLoginType.PASSWORD,
+        identifier: email,
+        credential: hashPassword(password),
+    });
+
+    const superAccount = new Account({
+        name,
+        email,
+        enabled: true,
+        email_verified: true,
+        auths: [accountAuth],
+        roles: [superRole],
+    });
+
+    const findConflict = await accountRepository.findOne({
+        where: { email },
+        relations: {
+            auths: true,
+            roles: true, 
+        },
+        withDeleted: false,
+    });
+
+    if (!findConflict) {
+        await accountRepository.save(superAccount);
+    }
+};
 
 export class AccountSeeder implements Seeder {
     public async run(
         dataSource: DataSource,
     ) {
         const roleRepository = dataSource.getRepository(Role);
-        const roles = Object.values(EnumRole);
-
-        for (const role of roles) {
-            const newRole = new Role({ role });
-            await roleRepository.upsert(newRole, ['role']);
-        }
-
         const accountRepository = dataSource.getRepository(Account);
 
-        const name = 'super';
-        const email = process.env.SUPER_EMAIL || 'super@todoless.com';
-        const password = process.env.SUPER_PASSWORD
-            ? hashPassword(process.env.SUPER_PASSWORD)
-            : hashPassword('super123');
-
-        const accountRole = await roleRepository.findOneBy({ role: EnumRole.SUPER });
-
-        const accountAuth = new AccountAuth({
-            type: EnumLoginType.PASSWORD,
-            identifier: email,
-            credential: password,
-        });
-
-        const superAccount = new Account({
-            name,
-            email,
-            enabled: true,
-            email_verified: true,
-            auths: [accountAuth],
-            roles: [accountRole],
-        });
-
-        const findConflict = await accountRepository.findOne({
-            where: { email },
-            relations: {
-                auths: true,
-                roles: true, 
-            }, 
-        });
-
-        if (!findConflict) {
-            await accountRepository.save(superAccount);
-        } else {
-            findConflict.auths[0].credential = password;
-            findConflict.roles = superAccount.roles;
-
-            await accountRepository.save(findConflict);
-        }
+        await seedRoles(roleRepository);
+        await seedSuperAccount(accountRepository, roleRepository);
     }
 }
